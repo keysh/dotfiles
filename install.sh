e88f59c9feb0672cf721710c0929a67165e90e52
Jakub Leško <jakub.lesko@protonmail.com>
2017-01-23 22:41:25 +0100

    Prompt for git user/email details

--- install.sh
+++ install.sh
@@ -11,7 +11,26 @@ cp ./bash/input.sh   ~/.inputrc
 cp ./git/config.git  ~/.gitconfig
 cp ./git/ignore.git  ~/.gitignore
 
+function git_details {
+    read -p "What is your user name? " user;
+    read -p "What is your email? " email;
+
+    echo "[user]" >> ~/.gitconfig;
+    echo "    name  =" $user >> ~/.gitconfig;
+    echo "    email =" $email >> ~/.gitconfig;
+}
+
+read -p "Do you wish to configure git user/email? [Y/n] " git
+case $git in
+    ""|y|Y*)
+        git_details;
+        ;;
+    *)
+        echo "Git details skipped.";
+        ;;  
+esac
+
 # Vim configuration
 cp ./vim/config.vim  ~/.vimrc
 
-echo "Configuration files were successfully installed."
+printf "\n[OK] Configuration files were successfully installed.\n"
