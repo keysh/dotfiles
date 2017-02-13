76805a2bee95a4aca2bbeaf3522ff085ea277688
Jakub Leško <jakub.lesko@protonmail.com>
2017-02-13 20:23:27 +0100

    Profile tweaks

--- bash/profile.sh
+++ bash/profile.sh
@@ -4,17 +4,24 @@
 source ~/.bash_prompt
 source ~/.bash_aliases
 
+# Autocorrect typos in path names when using `cd`
+shopt -s cdspell
+
 # Make vim the default editor
 export EDITOR="vim"
 
 # Ignore duplicate commands in the history
 export HISTCONTROL=ignoredups
 
-# Autocorrect typos in path names when using `cd`
-shopt -s cdspell
-
-# Cargo modules bin
-export PATH=$PATH:~/.cargo/bin
-
 # Rust source code (used with racer utility)
 export RUST_SRC_PATH=~/Documents/GitHub/rust/src
+
+# Java installation path
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
+
+# Android SDK
+export ANDROID_HOME=~/.local/share/android-sdk
+
+# Configure PATH
+PATH=$PATH:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.local/share/android-sdk/tools:$HOME/.local/share/android-sdk/platform-tools
+export PATH
